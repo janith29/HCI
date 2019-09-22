9230f3fcca9d5d33cb09e1f44820116dd32c00a3
janith29 <janithsandaruwan29@gmail.com>
2019-09-22 16:28:19 +0530

    Update 2019.09.22

--- js/feed.js
+++ js/feed.js
@@ -1,70 +1,149 @@
 function feedback() {
-    var nameobj = document.getElementById("namet");
-    var emailobj = document.getElementById("emailt");
-    var contactobj = document.getElementById("contactt");
-    var commentobj = document.getElementById("commentt");
-    
-    if (!nameobj.checkValidity()) {
-      var element = document.getElementById("namet");
+  var nameobj = document.getElementById("namet");
+  var emailobj = document.getElementById("emailt");
+  var contactobj = document.getElementById("contactt");
+  var commentobj = document.getElementById("commentt");
+  
+  if (!nameobj.checkValidity()) {
+    var element = document.getElementById("namet");
+    element.classList.remove("is-valid");
+    element.classList.add("is-invalid");
+    document.getElementById("namep").innerHTML = nameobj.validationMessage;
+  }
+  if (nameobj.checkValidity()) {
+    var element = document.getElementById("namet");
+    element.classList.remove("is-invalid");
+    element.classList.add("is-valid");
+    document.getElementById("namep").innerHTML = "Ok";
+  }
+  
+  if (!emailobj.checkValidity()) {
+    var element = document.getElementById("emailt");
+    element.classList.remove("is-valid");
+    element.classList.add("is-invalid");
+    document.getElementById("emailp").innerHTML = emailobj.validationMessage;
+  }
+  if (emailobj.checkValidity()) {
+    var element = document.getElementById("emailt");
+    element.classList.remove("is-invalid");
+    element.classList.add("is-valid");
+    document.getElementById("emailp").innerHTML = "Ok";
+  }
+  if (!contactobj.checkValidity()) {
+    var element = document.getElementById("contactt");
+    element.classList.remove("is-valid");
+    element.classList.add("is-invalid");
+    document.getElementById("contactp").innerHTML = contactobj.validationMessage;
+  }
+  if (contactobj.checkValidity()) {
+    var element = document.getElementById("contactt");
+    element.classList.remove("is-invalid");
+    element.classList.add("is-valid");
+    document.getElementById("contactp").innerHTML = "Ok";
+  }
+  var phoneno = /^\+?([0-9]{2})\)?[-. ]?([0-9]{4})[-. ]?([0-9]{4})$/;
+  if((contactobj.value.match(phoneno))){
+    var element = document.getElementById("contactt");
+    element.classList.remove("is-invalid");
+    element.classList.add("is-valid");
+    document.getElementById("contactp").innerHTML = "Ok";
+  }
+  if((!contactobj.value.match(phoneno))){
+    var element = document.getElementById("contactt");
+    element.classList.remove("is-invalid");
+    element.classList.add("is-invalid");
+    document.getElementById("contactp").innerHTML = "Not valid Phone number.Ex:-'0123456789'";
+  }
+  if (!commentobj.checkValidity()) {
+      var element = document.getElementById("commentt");
       element.classList.remove("is-valid");
       element.classList.add("is-invalid");
-      document.getElementById("namep").innerHTML = nameobj.validationMessage;
+      document.getElementById("commentp").innerHTML = commentobj.validationMessage;
     }
-    if (nameobj.checkValidity()) {
-      var element = document.getElementById("namet");
+    if (commentobj.checkValidity()) {
+      var element = document.getElementById("commentt");
       element.classList.remove("is-invalid");
       element.classList.add("is-valid");
+      document.getElementById("commentp").innerHTML = "Ok";
     }
-    
-    if (!emailobj.checkValidity()) {
-      var element = document.getElementById("emailt");
+  
+  if((nameobj.checkValidity())&& (emailobj.checkValidity())&&(contactobj.checkValidity())&&(commentobj.checkValidity())&&((contactobj.value.match(phoneno)))){
+    alert("Successful add feedback details!!!");
+    window.location = '/feed.html';
+  }
+} 
+
+function contactback() {
+  var nameobj = document.getElementById("namet");
+  var emailobj = document.getElementById("emailt");
+  var contactobj = document.getElementById("contactt");
+  var commentobj = document.getElementById("commentt");
+  
+  if (!nameobj.checkValidity()) {
+    var element = document.getElementById("namet");
+    element.classList.remove("is-valid");
+    element.classList.add("is-invalid");
+    document.getElementById("namep").innerHTML = nameobj.validationMessage;
+  }
+  if (nameobj.checkValidity()) {
+    var element = document.getElementById("namet");
+    element.classList.remove("is-invalid");
+    element.classList.add("is-valid");
+    document.getElementById("namep").innerHTML = "Ok";
+  }
+  
+  if (!emailobj.checkValidity()) {
+    var element = document.getElementById("emailt");
+    element.classList.remove("is-valid");
+    element.classList.add("is-invalid");
+    document.getElementById("emailp").innerHTML = emailobj.validationMessage;
+  }
+  if (emailobj.checkValidity()) {
+    var element = document.getElementById("emailt");
+    element.classList.remove("is-invalid");
+    element.classList.add("is-valid");
+    document.getElementById("emailp").innerHTML = "Ok";
+  }
+  if (!contactobj.checkValidity()) {
+    var element = document.getElementById("contactt");
+    element.classList.remove("is-valid");
+    element.classList.add("is-invalid");
+    document.getElementById("contactp").innerHTML = contactobj.validationMessage;
+  }
+  if (contactobj.checkValidity()) {
+    var element = document.getElementById("contactt");
+    element.classList.remove("is-invalid");
+    element.classList.add("is-valid");
+    document.getElementById("contactp").innerHTML = "Ok";
+  }
+  var phoneno = /^\+?([0-9]{2})\)?[-. ]?([0-9]{4})[-. ]?([0-9]{4})$/;
+  if((contactobj.value.match(phoneno))){
+    var element = document.getElementById("contactt");
+    element.classList.remove("is-invalid");
+    element.classList.add("is-valid");
+  }
+  if((!contactobj.value.match(phoneno))){
+    var element = document.getElementById("contactt");
+    element.classList.remove("is-invalid");
+    element.classList.add("is-invalid");
+    document.getElementById("contactp").innerHTML = "Not valid Phone number.Ex:-'0123456789'";
+  }
+  if (!commentobj.checkValidity()) {
+      var element = document.getElementById("commentt");
       element.classList.remove("is-valid");
       element.classList.add("is-invalid");
-      document.getElementById("emailp").innerHTML = emailobj.validationMessage;
+      document.getElementById("commentp").innerHTML = commentobj.validationMessage;
     }
-    if (emailobj.checkValidity()) {
-      var element = document.getElementById("emailt");
+    if (commentobj.checkValidity()) {
+      var element = document.getElementById("commentt");
       element.classList.remove("is-invalid");
       element.classList.add("is-valid");
+      document.getElementById("commentp").innerHTML = "Ok";
     }
-    if (!contactobj.checkValidity()) {
-      var element = document.getElementById("contactt");
-      element.classList.remove("is-valid");
-      element.classList.add("is-invalid");
-      document.getElementById("contactp").innerHTML = contactobj.validationMessage;
-    }
-    if (contactobj.checkValidity()) {
-      var element = document.getElementById("contactt");
-      element.classList.remove("is-invalid");
-      element.classList.add("is-valid");
-    }
-    var phoneno = /^\+?([0-9]{2})\)?[-. ]?([0-9]{4})[-. ]?([0-9]{4})$/;
-    if((contactobj.value.match(phoneno))){
-      var element = document.getElementById("contactt");
-      element.classList.remove("is-invalid");
-      element.classList.add("is-valid");
-    }
-    if((!contactobj.value.match(phoneno))){
-      var element = document.getElementById("contactt");
-      element.classList.remove("is-invalid");
-      element.classList.add("is-invalid");
-      document.getElementById("contactp").innerHTML = "Not valid Phone number.Ex:-'0123456789'";
-    }
-    if (!commentobj.checkValidity()) {
-        var element = document.getElementById("commentt");
-        element.classList.remove("is-valid");
-        element.classList.add("is-invalid");
-        document.getElementById("commentp").innerHTML = commentobj.validationMessage;
-      }
-      if (commentobj.checkValidity()) {
-        var element = document.getElementById("commentt");
-        element.classList.remove("is-invalid");
-        element.classList.add("is-valid");
-      }
-    
-    if((nameobj.checkValidity())&& (emailobj.checkValidity())&&(contactobj.checkValidity())&& (departmentobj.checkValidity())&&((contactobj.value.match(phoneno)))){
-      alert("Successful add feedback details!!!");
-      window.location = '/feed.html';
-    }
-  } 
+  
+    if((nameobj.checkValidity())&& (emailobj.checkValidity())&&(contactobj.checkValidity())&&(commentobj.checkValidity())&&((contactobj.value.match(phoneno)))){
+      alert("Successful add contact details!!!");
+    window.location = '/contact.html';
+  }
+} 
 
